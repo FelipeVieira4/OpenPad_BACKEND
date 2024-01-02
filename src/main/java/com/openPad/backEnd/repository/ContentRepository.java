e9fbb6df0c5ffcb2f8753e983ef85cfee94a7bf8
FelipeVieira4 <felipevieira58238@gmail.com>
2024-01-02 11:48:27 -0300

    update: removido ContentFiles

--- src/main/java/com/openPad/backEnd/repository/ContentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.openPad.backEnd.repository;
-
-import javax.swing.text.html.parser.ContentModel;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContentRepository extends JpaRepository<ContentModel,String>{}
